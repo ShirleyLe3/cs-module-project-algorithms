bee53d8da42356c601ba5fe9ab607fe89522902e
Drew Ross <mail.drew.ross@gmail.com>
2020-09-16 12:26:50 -0700

    Complete moving_zeroes. Test OK.

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,13 +3,18 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    result, zeroes = [], []
+    for num in arr:
+        if num != 0:
+            result.append(num)
+        else:
+            zeroes.append(num)
+    result += zeroes
+    return result
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+    arr = [0, 0, 0, 0, 3, 2, 1]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
