e6c1044fa45a36c37b874a96832832e483f73676
Drew Ross <mail.drew.ross@gmail.com>
2020-09-17 11:08:47 -0700

    Complete eating_cookies. Test OK. Add alternative functions to single_number.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,14 +1,24 @@
-'''
+"""
 Input: an integer
 Returns: an integer
-'''
-def eating_cookies(n):
-    # Your code here
+"""
 
-    pass
+def eating_cookies(n):
+    total = 0
+    count = [1, 1, 2]
+    if n < 3:
+        return count[n]
+    for i in range(n - 2):
+        total = sum(count)
+        count.pop(0)
+        count.append(total)
+    return total
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies"
+    )
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -12,6 +12,21 @@ def single_number(arr):
     (single,) = set.difference(setA, setB)
     return single
 
+# def single_number(arr):
+#     s = set()
+#     for num in arr:
+#         if num in s:
+#             s.remove(num)
+#         else:
+#             s.add(num)
+#     (single,) = s
+#     return single
+
+# def single_number(arr):
+#     for num in arr:
+#         if arr.count(num) == 1:
+#             return num
+
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
